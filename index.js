46e5ca88103f9e254d35cc01a2f862814d393a1a
Francisco Presencia Fandos <developerfrancisco@hotmail.com>
2017-04-10 20:10:09 +0200

    Updated to export other params

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const request = require('request');
 module.exports = opts => new Promise((resolve, reject) => {
   request(opts, (err, res) => err ? reject(err) : resolve(res));
 });
+
+for (let key in request) {
+  module.exports[key] = request[key];
+}
